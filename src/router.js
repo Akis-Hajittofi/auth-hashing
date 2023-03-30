37dc51a039a71665c7ebf280f8ed0f9bd4380c78
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-03-30 11:57:56 +0100

    Both requirements completed

--- src/router.js
+++ src/router.js
@@ -1,17 +1,49 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-
+const secret = process.env.JWT_SECRET;
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-    
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 10);
+
+  try {
+    const user = await prisma.user.create({
+      // data: { username, password: hash }
+      // Maybe in bigger use cases, there will be more data,
+      // so the solution below could be better
+
+      // Anything after the comma won't be included in the spread syntax
+      data: { ...req.body, password: hash },
+    });
+    res.json({ user: { id: user.id, username: user.username, hash: hash } });
+  } catch (error) {
+    res.status(500).json({ error: "Cannot be registered!" });
+  }
 });
 
-router.post('/login', async (req, res) => {
-    
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const checkUser = await prisma.user.findUnique({
+    where: { username: username },
+  });
+
+  const checkPasswordsMatch = await bcrypt.compare(
+    password,
+    checkUser.password
+  );
+
+  if (!checkUser) {
+    res.status(401).json({ error: "Username invalid!" });
+  } else if (!checkPasswordsMatch) {
+    res.status(401).json({ error: "Password invalid!" });
+  } else {
+    res.json(jwt.sign(checkUser.username, secret));
+  }
 });
 
 module.exports = router;
